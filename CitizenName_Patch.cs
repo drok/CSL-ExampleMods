6d2c0072476acd9c269b2e410552f00ab6b16197
Radu Hociung <radu.csmods@ohmi.org>
2021-04-10 17:32:15 -0400

    Add typical Gameplay changing mod
    This mod uses Harmony to patch the CitizenAI class in order to change
    the lastnames of 25% of citizen instances, selected pseudo-randomly, to
    "Modmaker"
    
    It demonstrates:
    - mod lifecycle (auto-subscribing Harmony, clean patch installation
      and teardown)
    - attribute-based patching using the HarmonyLib library
    
    The Harmony library itself is supplied into the game environment by
    the Harmony mod, which is installed if necessary when
    HarmonyManager.Harmony.DoOnHarmonyReady() is called.

---
+++ CitizenName_Patch.cs
@@ -0,0 +1,61 @@
+﻿/*
+ * Example Game Mod - An example mod that makes the last names of 25% of cims' be "Modmaker"
+ *
+ * Written in 2021 by Radu Hociung (radu.csmods@ohmi.org)
+ *
+ * To the extent possible under law, the author(s) have dedicated all copyright and related
+ * and neighboring rights to this software to the public domain worldwide. This software is
+ * distributed without any warranty.
+ *
+ * You should have received a copy of the CC0 Public Domain Dedication along with this
+ * software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
+/* This file contains patches to be installed on the Cities Skylines game
+ * assembly.
+ * It implements annotations that direct the Harmony Library's patching process.
+ * Please see documentation on the annotations and the workings of the
+ * Harmony Library at the author's manual webpage at
+ * https://harmony.pardeike.net/articles/annotations.html
+ *
+ */
+
+
+namespace ExampleMod.Patch
+{
+    using HarmonyLib;
+    using ColossalFramework.Math;
+
+    [HarmonyPatch(typeof(CitizenAI), "GenerateCitizenName")]
+    internal class CitizenAI_GenerateCitizenName_Patch
+    {
+        public static bool Prefix(CitizenAI __Instance, uint citizenID, byte family, ref string __result)
+        {
+            Randomizer randomizer = new Randomizer(citizenID);
+            Randomizer randomizer2 = new Randomizer(family);
+            string firstName;
+            string lastName;
+            if (Citizen.GetGender(citizenID) == Citizen.Gender.Male)
+            {
+                firstName = "NAME_MALE_FIRST";
+                lastName = "NAME_MALE_LAST";
+            } else
+            {
+                firstName = "NAME_FEMALE_FIRST";
+                lastName = "NAME_FEMALE_LAST";
+
+            }
+            firstName = ColossalFramework.Globalization.Locale.Get(firstName, randomizer.Int32(ColossalFramework.Globalization.Locale.Count(firstName)));
+            if (randomizer.Int32(4) == 0)
+            {
+                lastName = "Modmaker";
+            } else
+            {
+                lastName = ColossalFramework.Globalization.Locale.Get(lastName, randomizer2.Int32(ColossalFramework.Globalization.Locale.Count(lastName)));
+            }
+            __result = StringUtils.SafeFormat(lastName, firstName);
+
+            return false;
+        }
+    }
+}
---
+++ ExampleMod.cs
@@ -0,0 +1,96 @@
+﻿/*
+ * Example Game Mod - An example mod that makes the last names of 25% of cims' be "Modmaker"
+ *
+ * Written in 2021 by Radu Hociung (radu.csmods@ohmi.org)
+ *
+ * To the extent possible under law, the author(s) have dedicated all copyright and related
+ * and neighboring rights to this software to the public domain worldwide. This software is
+ * distributed without any warranty.
+ *
+ * You should have received a copy of the CC0 Public Domain Dedication along with this
+ * software. If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.
+ */
+
+using ICities;
+
+namespace ExampleGameMod
+{
+    /*
+     * This is mod that changes the behaviour of the game. It changes the last names
+     * of some of the cims (a pseudo-random 25% sampling) to "ModMaker".
+     * It demonstrates attribute-driven patching using HarmonyLib to
+     * patch the CitizenAI.GenerateCitizenName() method and override it.
+     *
+     * If Harmony is not already installed, it will be automatically installed and the
+     * patch will be done when Harmony is ready.
+     *
+     * The mod demonstrates simple mod lifecycle:
+     * - it operates only when Game mode starts (ie, when a city is loaded)
+     * - it removes the patching callback request in case Harmony install is delayed or fails
+     * - it removes its own patches when Game mode ends.
+     * - it removes itself cleanly when the assembly file is deleted (ie, it can be unsubscribed
+     *   or deleted mid-game)
+     */
+    public class ExampleMod : LoadingExtensionBase, IUserMod
+    {
+        public string Name => "Example Cim Name Mod";
+
+        public string Description => "Change some cim last names to 'Modmaker'";
+
+        /* This object is a HarmonyLib.Harmony object but it must be declared as a generic
+         * object so it does not need to be resolved on GetExportedTypes(), or it will
+         * cause the legacy plugin manager to crash.
+         */
+        object patcher;
+
+        /* When the mod assembly is removed by unsubscribing or deleting the mod from
+         * the disk, the plugin manager calls OnDisabled().
+         *
+         * Upon this trigger, the mod will cleanly unload.
+         */
+        public void OnDisabled()
+        {
+            Unload();
+        }
+
+        /* LoadingExtension.OnCreated() is called when the player starts an appmode
+         * by loading a city, the asset editor or a map editor.
+         */
+        public override void OnCreated(ILoading loading)
+        {
+            if (loading.currentMode == AppMode.Game)
+            {
+                HarmonyManager.Harmony.DoOnHarmonyReady(PatchGame);
+            }
+            base.OnCreated(loading);
+        }
+
+        /* LoadingExtension.OnReleased() is called when the player
+         * returns to the main menu.
+         */
+        public override void OnReleased()
+        {
+            Unload();
+            base.OnReleased();
+        }
+
+        void PatchGame()
+        {
+            if (patcher == null)
+            {
+                /* On first call, allocate the Harmony instance */
+                patcher = new HarmonyLib.Harmony(Name);
+            }
+            (patcher as HarmonyLib.Harmony).PatchAll();
+        }
+
+        void Unload()
+        {
+            /* If Harmony installation is still pending, cancel the callback */
+            HarmonyManager.Harmony.CancelOnHarmonyReady();
+
+            /* If patching was done, remove it */
+            (patcher as HarmonyLib.Harmony)?.UnpatchAll(Name);
+        }
+    }
+}
